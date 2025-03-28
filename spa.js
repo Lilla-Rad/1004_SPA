83a28dbdced6cc422617bc96f04066db4fdf4110
Lilla-Rad <lilla.radek@students.plymouth.ac.uk>
2025-03-28 08:16:59 +0000

    Add files via upload

---
+++ spa.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('wwwroot/index.html');
+    })
+})
+
+$(document).ready(function () {
+    $('#faq').click(function () {
+        $('#page-content-wrapper').load('wwwroot/faq.html');
+    })
+})
+
+$(document).ready(function () {
+    $('#note').click(function () {
+        $('#page-content-wrapper').load('wwwroot/note.html');
+    })
+})
+
+$(document).ready(function () {
+    $('#game').click(function () {
+        $('#page-content-wrapper').load('wwwroot/game.html');
+    })
+})
